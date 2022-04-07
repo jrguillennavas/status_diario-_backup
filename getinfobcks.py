f757e2a472b85600c91b5d35324a6b2493796cde
Jose Guillen <68676892+jrguillennavas@users.noreply.github.com>
2022-04-07 16:22:04 -0400

    Update getinfobcks.py
    Add key 'folder' to dic

--- getinfobcks.py
+++ getinfobcks.py
@@ -23,7 +23,7 @@ def info_backup(path_bck):
             route_file = f'{route}/{file}'
             if datetime.fromtimestamp(os.path.getatime(route_file)).strftime(f'%Y%m%d') == datetime.now().date().strftime(f'%Y%m%d'):
                 exists = True
-                data += [{'backup': True, 'su':f'{split_route[-1] if split_route[-2] == "Backup" else split_route[-2]}'.upper(), 'folder': f'{split_route[-1]}', 'name': f'{file}', 'size': f'{round(os.path.getsize(route_file) / 1024**3)}GB', 'date': f'{datetime.fromtimestamp(os.path.getatime(route_file))}'} if len(split_route) > 3 else {'backup': True, 'su':f'{split_route[-1]}', 'name': f'{file}', 'size': f'{round(os.path.getsize(route_file) / 1024**3)}GB', 'date': f'{datetime.fromtimestamp(os.path.getatime(route_file))}'}]
+                data += [{'backup': True, 'su':f'{split_route[-1] if split_route[-2] == "Backup" else split_route[-2]}'.upper(), 'folder': f'{split_route[-1]}', 'name': f'{file}', 'size': f'{round(os.path.getsize(route_file) / 1024**3)}GB', 'date': f'{datetime.fromtimestamp(os.path.getatime(route_file))}'} if len(split_route) > 3 else {'backup': True, 'su':f'{split_route[-2]}', 'folder': f'{split_route[-1]}', 'name': f'{file}', 'size': f'{round(os.path.getsize(route_file) / 1024**3)}GB', 'date': f'{datetime.fromtimestamp(os.path.getatime(route_file))}'}]
             
         if not exists and len(split_route) > 3:
             data += [{'backup': False, 'su':f'{split_route[-2]}'.upper(), 'folder': f'{split_route[-1]}', 'name': 'Fallo', 'size': '0GB', 'date': f'{ datetime.now()}'}]
